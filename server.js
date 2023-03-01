8d9dbf7d23ca4eccafc6c974adeaaecc0da85720
mushroom <shaelah.mai@gmail.com>
2023-03-01 16:12:37 +1000

    debugging server.js

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ app.use(express.static('public'));
 
 // GET Route for notes page
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, './public/notes.html'))
 });
 
 // GET Route for homepage 
 app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, './public/index.html'))
   );
 
 app.listen(PORT, () => {
